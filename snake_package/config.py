eec3b2d57ad48f4b8f9f4303b36128f58419ef09
Арсений <130998799+akseniy@users.noreply.github.com>
2024-11-22 13:05:36 +0300

    Add files via upload

---
+++ snake_package/config.py
@@ -0,0 +1,19 @@
+"""Этот модуль содержит константы необходимые для работы main.py"""
+
+
+white = (255, 255, 255)
+yellow = (255, 255, 102)
+black = (0, 0, 0)
+red = (213, 50, 80)
+green = (0, 255, 0)
+dark_green = (50, 153, 100)
+deep_blue = (0, 0, 255)
+WIDTH = 1000
+HEIGHT = 760
+
+steps = 20
+snake_speed = 15
+base_snake_speed = 15
+snake_speed_up = 10
+snake_speed_down = 20
+FPS = 15
